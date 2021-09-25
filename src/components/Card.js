8b0be677c1ef37fa1dae3fbc5d58a1835a479c88
Devin Jaggernauth <neexis@gmail.com>
2021-09-25 11:32:16 -0400

    cards display

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+function Card(props){
+    return(
+    props.cards.map(card=>(
+        <article className="card" key={card._id}>
+          <button className="card__delete-button" type="button" aria-label="Delete card"></button>
+          <img className="card__image" src={card.link} alt="" />
+          <div className="card__items">
+            <h2 className="card__title">{card.name}</h2>
+            <div className="card__likes">
+              <button className="card__like-button" type="button" aria-label="Like this"></button>
+              <p className="card__like-count">{card.likes.length}</p>
+            </div>
+          </div>
+        </article>
+    )))
+}
+
+export default Card
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,50 @@
+import React from "react";
+import { api } from "../utils/api";
+import Card from "./Card";
+
 function Main(props) {
+
+const [userName, setUserName] = React.useState('');
+const [userDescription, setUserDescription] = React.useState('');
+const [userAvatar, setUserAvatar] = React.useState('');
+const [cards, setCards] = React.useState([])
+
+React.useEffect(()=>{
+  api.getProfileInfo()
+  .then((res)=>{
+    setUserName(res.name);
+    setUserDescription(res.about);
+  })
+  },[userName])
+
+  React.useEffect(()=>{
+    api.getProfileInfo()
+    .then((res)=>{
+      setUserAvatar(res.avatar);
+    })
+  }, [userAvatar])
+
+  React.useEffect(()=>{
+    api.getCards()
+    .then((res)=>{
+      setCards(res);
+    })
+  }, [userAvatar])
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar">
-          <img src=" " alt="User profile pic" className="profile__image" />
+          <img src={userAvatar} alt="User profile pic" className="profile__image" />
           <button className="profile__edit-image" type="button" aria-label="Edit User Photo"
             onClick={props.onEditAvatarClick}></button>
         </div>
         <div className="profile__info">
           <div className="profile__title">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userName}</h1>
             <button className="profile__edit-btn hover-animate" type="button" aria-label="Edit profile" onClick={props.onEditProfileClick}></button>
           </div>
-          <p className="profile__subtitle"></p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button className="profile__add-button hover-animate" aria-label="Add" type="button"
           onClick={props.onAddPlaceClick}></button>
@@ -20,6 +52,7 @@ function Main(props) {
 
 
       <section className="elements">
+      <Card cards={cards}></Card>
       </section>
 
     </main>
